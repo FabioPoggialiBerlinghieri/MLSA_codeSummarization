413e081b6fbcadefc537c08bca12c4aaea7d3f96
piergiorgiodanti <piergiorgio.danti@edu.unifi.it>
2026-09-09 12:12:55 +0200

    fixed predict logic

--- src/EDTransf.py
+++ src/EDTransf.py
@@ -38,9 +38,9 @@ class EDTransf(nn.Module):
         # inputs: B (=1) x L_in
         # inputs_mask: B (=1) x L_in
         # current_seq: B (=1) x L_lab (=1)
-        inputs = torch.tensor([inputs], dtype=torch.long, device=inputs.device)
-        input_mask = torch.tensor([input_mask], dtype=torch.long, device=inputs.device)
-        current_seq = torch.tensor([[cls]], dtype=torch.long, device=inputs.device)
+        inputs = inputs.unsqueeze(0)
+        input_mask = input_mask.unsqueeze(0)
+        current_seq = torch.tensor(cls).unsqueeze(0).unsqueeze(0)
 
         # preprocessed_inputs: B x L_in x D_emb
         preprocessed_inputs = self.preprocess_inputs(inputs)
