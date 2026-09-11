d1a269f184fdf2f77945056f3a64b11fbc4db3eb
piergiorgiodanti <piergiorgio.danti@edu.unifi.it>
2026-09-11 10:41:04 +0200

    fix bug dataset handler

--- src/main.py
+++ src/main.py
@@ -48,7 +48,6 @@ class DatasetHandler:
         if self.config_yaml['first_time'] is True:
             self.modified = True
             self.config_yaml['first_time'] = False
-            self.__save_config()
 
         self.codeTokenizer = None
         self.text_padding_handler = None
