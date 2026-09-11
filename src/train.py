11a255f1c8636f6e72a0ef440662c704bcacaf1b
piergiorgiodanti <piergiorgio.danti@edu.unifi.it>
2026-09-11 14:44:50 +0200

    Rimosso tracciamento della cartella models

--- src/train.py
+++ src/train.py
@@ -9,7 +9,6 @@ from paddingMask import PaddingMask
 import argparse
 import torch
 import evaluate as e
-import subprocess
 
 bleu = e.load("bleu")
 
@@ -103,7 +102,6 @@ class ModelTrainer:
                         'step': step
                     }
                     torch.save(checkpoint_latest, self.dataset_handler.config_yaml['checkpoint_path'])
-                    self.__push()
                     print("Checkpoint saved...")
 
             training_loss /= len(self.train_dataset)
@@ -144,7 +142,6 @@ class ModelTrainer:
                     'model_state_dict': self.model.state_dict()
                 }
                 torch.save(checkpoint_loss, self.dataset_handler.config_yaml['best_loss_path'])
-                self.__push()
                 print(f"New best loss saved ({valid_loss}) ...")
 
             sample_batch = next(iter(val_loader))
@@ -168,17 +165,11 @@ class ModelTrainer:
                     'model_state_dict': self.model.state_dict()
                 }
                 torch.save(checkpoint_loss, self.dataset_handler.config_yaml['best_bleu_path'])
-                self.__push()
                 print(f"New best bleu saved ({best_bleu}) ...")
 
 
             print('Epoch: {}, Training Loss: {:.4f}, Validation Loss: {:.4f}, accuracy = {:.4f}, bleu = {:.4f}'.format(
                 epoch,training_loss, valid_loss, num_correct / num_examples, bleu_result))
-
-    def __push(self):
-        subprocess.run(["git", "add", "-A"], check=True)
-        subprocess.run(["git", "commit", "-m", "'Update weights'"], check=True)
-        subprocess.run(["git", "push"], check=True)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="TBD")
